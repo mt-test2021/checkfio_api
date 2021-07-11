df023e85d61b1af0b4d511556461c97a2664164e
mt-test2021 <test-tkb2021@yandex.ru>
2021-07-11 16:41:15 +0400

    Добавлен функционал сохранения данных

--- app/Http/Controllers/FIOController.php
+++ app/Http/Controllers/FIOController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Rules\Cyrillic;
+use Illuminate\Support\Facades\DB;
 
 class FIOController extends Controller
 {
@@ -23,6 +24,35 @@ class FIOController extends Controller
         if ($validator->fails()) {
             return json_encode(["success" => false, "message" => $validator->errors() ]);
         }
+
+        return json_encode(["success" => true]);
+    }
+
+    public function saveFIO(Request $request)
+    {
+        $checkFIO = $this->checkFIO($request);
+
+        if(json_decode($checkFIO)->success !== true) {
+            return $checkFIO;
+        }
+
+        $fio = $request->all();
+
+        $existing_fio = DB::table('fio')
+                            ->where('last_name', $fio['last_name'])
+                            ->where('first_name', $fio['first_name']);
+
+        if(isset($fio['surname'])) {
+            $existing_fio = $existing_fio->where('surname', $fio['surname']);
+        } else {
+            $existing_fio = $existing_fio->where('surname', null);
+        }
+
+        if($existing_fio->first() != null) {
+            return json_encode(["success" => false, "message" => ["unique_check" => "Такие ФИО уже существуют."]]);
+        }
+
+        DB::table('fio')->insert($fio);
 
         return json_encode(["success" => true]);
     }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/checkfio', [FIOController::class, 'checkFIO']);
+Route::post('/savefio', [FIOController::class, 'saveFIO']);
